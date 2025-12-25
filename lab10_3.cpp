e667237e14a86cf159796b925cf16c44e76f7a17
GAME <naravitwongcome@gmail.com>
2025-12-26 05:40:01 +0700

    lab10_3_complete.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,35 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream> 
+
+using namespace std;
+
+int main()
+{
+        int count = 0;
+        float sum = 0;
+        float sum_of_square = 0;
+        string textline;
+        ifstream source("score.txt"); 
+        
+        while (getline(source, textline)) 
+        {
+                float val = atof(textline.c_str()); 
+                sum += val;
+                sum_of_square += val * val;
+                count++;
+        }
+        
+        float mean = sum / count; 
+        float sd = sqrt((sum_of_square / count) - (mean * mean)); 
+
+        cout << "Number of data = " << count << "\n";
+        cout << setprecision(3);
+        cout << "Mean = " << mean << "\n";
+        cout << "Standard deviation = " << sd << endl;
+        
+        source.close();
+        return 0;
+}
